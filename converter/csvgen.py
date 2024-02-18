78ec4095ddac0ed97fcca06fb636c9de0b290e70
Rob Wyse <r.wyse@vivaldi.net>
2024-02-18 19:19:15 +0100

    Update csvgen.py

--- converter/csvgen.py
+++ converter/csvgen.py
@@ -15,8 +15,19 @@ def generate_gias_csv(import_file: str, export_file: str, config_file: str):
     # Import mapping
     # Required fields
     col_inventarisnummer = config['ColumnInventarisnummer'] if config['ColumnInventarisnummer'] != "" else ""
-    col_benaming = config['ColumnBenamingArchiefeenheid'] if config['ColumnBenamingArchiefeenheid'] != "" else raise ValueError("A column for 'Benaming archiefeenheid' is required.")
+    if config['ColumnBenamingArchiefeenheid'] != "":
+        col_benaming = config['ColumnBenamingArchiefeenheid']
+    else:
+        raise ValueError("A column for 'benaming archiefeenheid' is required.")
+    col_soortBenaming = config["ColumnSoortBenaming"] if config["ColumnSoortBenaming"] != "" else ""
+    if config['ColumnPeriodeStartVanaf'] != "":
+        col_periodeStart = config['ColumnPeriodeStartVanaf']
+    else:
+        raise ValueError("A column for 'periode start vanaf' is required.")
 
+
+    # Required values
+    val_benamingGeautoriseerd = config["ValueIsBenamingGeautoriseerd"] if config["ValueIsBenamingGeautoriseerd"] != "" else 1
 
     # Import CSV file with object numbers and optional extra information
     csv_contents = csv.DictReader(open(import_file, encoding=file_encoding))
@@ -27,20 +38,37 @@ def generate_gias_csv(import_file: str, export_file: str, config_file: str):
         fields = []
         fields.append("Inventarisnummer")
         fields.append("Benaming Archiefeenheid")
+        fields.append("Soort benaming")
+        fields.append("Is Benaming Geautoriseerd")
+
+        
+        fields.append("Periode Start Vanaf")
 
 
         # define dictionary for each content row
         export_row = {}
         export_row["Inventarisnummer"] = ""
         export_row["Benaming Archiefeenheid"] = ""
+        export_row["Soort benaming"] = ""
+        export_row["Is Benaming Geautoriseerd"] = ""
+
+
+        export_row["Periode Start Vanaf"] = ""
 
         # create CSV file
         writer = csv.DictWriter(output_file, fieldnames=fields)
         writer.writeheader()
 
         # create content rows
+        row_number = 1
         for import_row in csv_contents:
-            export_row["Inventarisnummer"] = import_row[col_inventarisnummer]
+            export_row["Inventarisnummer"] = import_row[col_inventarisnummer] if col_inventarisnummer != "" else row_number
             export_row["Benaming Archiefeenheid"] = import_row[col_benaming]
+            export_row["Soort benaming"] = import_row[col_soortBenaming] if col_soortBenaming != "" else "Toegekende Titel"
+            export_row["Is Benaming Geautoriseerd"] = val_benamingGeautoriseerd
+
+
+            export_row["Periode Start Vanaf"] = import_row[col_periodeStart]
 
             writer.writerow(export_row)
+            row_number += 1
